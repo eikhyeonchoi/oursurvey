854be5a1190d0b16bb8b1a37da41b055e49f2399
ikhyunchoi <eikhyeon.choi@gmail.com>
2022-06-28 20:57:07 +0900

    메일 발송 작업 및 gitignore 수정

---
+++ oursurvey/src/main/java/com/oursurvey/util/MailUtil.java
@@ -0,0 +1,30 @@
+package com.oursurvey.util;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.mail.internet.MimeMessage;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class MailUtil {
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String FROM;
+
+    public void sendMail(String to, String subject, String content) throws Exception {
+        MimeMessage mail = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mail, true, "UTF-8");
+        helper.setFrom(FROM);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(content, true);
+        mailSender.send(mail);
+    }
+}
